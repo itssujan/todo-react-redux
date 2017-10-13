16aece42f7072ba670aec68c14fc473a296e0406
Sujan Abraham <itssujan@gmail.com>
2017-10-13 14:28:27 -0400

    add babel webpack

--- src/js/App.js
+++ src/js/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React Redux Bootstrap</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Lets learn...
         </p>
       </div>
     );
